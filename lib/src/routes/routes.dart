7a9ee8c3093e1b7ce452553f3cca1536f575127b
vitalii boiko <vukelan@gmail.com>
2025-06-04 23:16:26 +0300

    home is already specified

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -3,9 +3,6 @@ import 'package:ltrainer/src/home.dart';
 import 'package:ltrainer/src/hiragana/page.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  '/': (BuildContext context) {
-    return HomePage();
-  },
   '/hiragana': (BuildContext context) {
     return HiraganaPage();
   },
