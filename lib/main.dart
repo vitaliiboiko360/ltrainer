dd132471cdc549c4b3f191afca110ee9e6a8deab
vitalii boiko <vukelan@gmail.com>
2025-06-05 23:20:11 +0300

    add provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ltrainer/src/app.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const App());
--- lib/src/hiragana/page.dart
+++ lib/src/hiragana/page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ltrainer/src/hiragana/button_group.dart';
 
 class HiraganaPage extends StatefulWidget {
   const HiraganaPage({super.key});
@@ -10,18 +11,6 @@ class HiraganaPage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<HiraganaPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  double _calcTop(int k) {
-    return 100 + (k * 30);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,44 +22,7 @@ class _MyHomePageState extends State<HiraganaPage> {
               color: Colors.grey.shade100,
               width: 400,
               height: 400,
-              child: Stack(
-                children: <Widget>[
-                  // TextButton(
-                  //   onPressed: () {},
-                  //   style: TextButton.styleFrom(fixedSize: Size(10, 10)),
-                  //   child: const Text('a'),
-                  // ),
-                  Positioned(
-                    left: 100.0,
-                    top: _calcTop(0),
-                    child: TextButton(
-                      onPressed: () {},
-                      // style: TextButton.styleFrom(fixedSize: Size(10, 10)),
-                      child: const Text('a'),
-                    ),
-                  ),
-                  Positioned(
-                    left: 90,
-                    top: _calcTop(1),
-                    child: TextButton(onPressed: () {}, child: const Text('i')),
-                  ),
-                  Positioned(
-                    left: 85,
-                    top: _calcTop(2),
-                    child: TextButton(onPressed: () {}, child: const Text('u')),
-                  ),
-                  Positioned(
-                    left: 80,
-                    top: _calcTop(3),
-                    child: TextButton(onPressed: () {}, child: const Text('e')),
-                  ),
-                  Positioned(
-                    left: 75,
-                    top: _calcTop(4),
-                    child: TextButton(onPressed: () {}, child: const Text('o')),
-                  ),
-                ],
-              ),
+              child: ButtonGroup(),
             ),
           ],
         ),
