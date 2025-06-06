e8375ae7fee960290738b9fbce63bc4316b9447f
vitalii boiko <vukelan@gmail.com>
2025-06-07 01:10:19 +0300

    state not working

--- lib/src/hiragana/button_letter.dart
+++ lib/src/hiragana/button_letter.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ButtonLetter extends StatefulWidget {
-  const ButtonLetter({super.key, required this.text, required this.onPressed});
+  ButtonLetter({super.key, required this.text, required this.onPressed});
 
   final String text;
   final VoidCallback onPressed;
+  bool selected = false;
 
   @override
   State<ButtonLetter> createState() => _ButtonLetterState();
@@ -12,14 +13,29 @@ class ButtonLetter extends StatefulWidget {
 
 class _ButtonLetterState extends State<ButtonLetter> {
   late final WidgetStatesController statesController;
-  bool selected = false;
 
-  static const ButtonStyle style = ButtonStyle(
+  ButtonStyle style = ButtonStyle(
+    padding: WidgetStateProperty.resolveWith<EdgeInsetsDirectional?>((
+      Set<WidgetState> states,
+    ) {
+      return EdgeInsetsDirectional.only(bottom: 10);
+    }),
+    shape: WidgetStateProperty.resolveWith<CircleBorder?>((
+      Set<WidgetState> states,
+    ) {
+      return CircleBorder();
+    }),
+    textStyle: WidgetStateProperty.resolveWith<TextStyle?>((
+      Set<WidgetState> states,
+    ) {
+      return TextStyle(fontSize: 22);
+    }),
     foregroundColor: WidgetStateProperty<Color?>.fromMap(<WidgetState, Color>{
       WidgetState.selected: Colors.white,
     }),
-    backgroundColor: WidgetStateProperty<Color?>.fromMap(<WidgetState, Color>{
+    backgroundColor: WidgetStateProperty<Color?>.fromMap({
       WidgetState.selected: Colors.indigo,
+      WidgetState.any: Color(0xfff2ecf2),
     }),
   );
 
@@ -27,22 +43,28 @@ class _ButtonLetterState extends State<ButtonLetter> {
   void initState() {
     super.initState();
     statesController = WidgetStatesController(<WidgetState>{
-      if (selected) WidgetState.selected,
+      if (widget.selected) WidgetState.selected,
     });
+  }
+
+  @override
+  void didUpdateWidget(ButtonLetter oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.selected != oldWidget.selected) {
+      statesController.update(WidgetState.selected, widget.selected);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
       onPressed: () {
+        setState(() {
+          widget.selected = !widget.selected;
+        });
         widget.onPressed();
       },
-      style: TextButton.styleFrom(
-        backgroundColor: Color(0xfff2ecf2),
-        padding: EdgeInsetsDirectional.only(bottom: 10),
-        shape: CircleBorder(),
-        textStyle: TextStyle(fontSize: 22),
-      ),
+      style: style,
       child: Text(widget.text),
     );
   }
