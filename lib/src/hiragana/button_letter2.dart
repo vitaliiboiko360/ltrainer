ecad82dc2e2815e75410a9e7aef57662b566958d
vitalii boiko <vukelan@gmail.com>
2025-06-07 01:30:02 +0300

    add button letter 2

---
+++ lib/src/hiragana/button_letter2.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class SelectableButton extends StatefulWidget {
+  const SelectableButton({
+    super.key,
+    required this.selected,
+    this.style,
+    required this.onPressed,
+    required this.child,
+  });
+
+  final bool selected;
+  final ButtonStyle? style;
+  final VoidCallback? onPressed;
+  final Widget child;
+
+  @override
+  State<SelectableButton> createState() => _SelectableButtonState();
+}
+
+class _SelectableButtonState extends State<SelectableButton> {
+  late final WidgetStatesController statesController;
+
+  @override
+  void initState() {
+    super.initState();
+    statesController = WidgetStatesController(<WidgetState>{
+      if (widget.selected) WidgetState.selected,
+    });
+  }
+
+  @override
+  void didUpdateWidget(SelectableButton oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.selected != oldWidget.selected) {
+      statesController.update(WidgetState.selected, widget.selected);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      statesController: statesController,
+      style: widget.style,
+      onPressed: widget.onPressed,
+      child: widget.child,
+    );
+  }
+}
+
+class ButtonLetter2 extends StatefulWidget {
+  const ButtonLetter2({super.key, required this.text});
+  final String text;
+
+  @override
+  State<ButtonLetter2> createState() => _ButtonLetter2State();
+}
+
+class _ButtonLetter2State extends State<ButtonLetter2> {
+  bool selected = false;
+
+  static ButtonStyle style = ButtonStyle(
+    padding: WidgetStateProperty.resolveWith<EdgeInsetsDirectional?>((
+      Set<WidgetState> states,
+    ) {
+      return EdgeInsetsDirectional.only(bottom: 10);
+    }),
+    shape: WidgetStateProperty.resolveWith<CircleBorder?>((
+      Set<WidgetState> states,
+    ) {
+      return CircleBorder();
+    }),
+    textStyle: WidgetStateProperty.resolveWith<TextStyle?>((
+      Set<WidgetState> states,
+    ) {
+      return TextStyle(fontSize: 22);
+    }),
+    foregroundColor: WidgetStateProperty<Color?>.fromMap(<WidgetState, Color>{
+      WidgetState.selected: Colors.white,
+    }),
+    backgroundColor: WidgetStateProperty<Color?>.fromMap({
+      WidgetState.selected: Colors.indigo,
+      WidgetState.any: Color(0xfff2ecf2),
+    }),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return SelectableButton(
+      selected: selected,
+      style: style,
+      onPressed: () {
+        setState(() {
+          selected = !selected;
+        });
+      },
+      child: Text(widget.text),
+    );
+  }
+}
