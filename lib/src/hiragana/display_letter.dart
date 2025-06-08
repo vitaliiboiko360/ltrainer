b1c743aa7925b8a51a9879aa862abfa20dd9b696
vitalii boiko <vukelan@gmail.com>
2025-06-08 23:15:18 +0300

    add conditional render method

--- lib/src/hiragana/display_letter.dart
+++ lib/src/hiragana/display_letter.dart
@@ -1,27 +1,28 @@
 import 'package:flutter/material.dart';
 
+const noShow = -1;
+
 class DisplayLetter extends StatefulWidget {
-  const DisplayLetter({super.key});
+  const DisplayLetter({super.key, required this.characterToShow});
+
+  final int characterToShow;
 
   @override
   State<DisplayLetter> createState() => _DisplayLetterState();
 }
 
 class _DisplayLetterState extends State<DisplayLetter> {
+  int characterToShow = noShow;
+
+  _getDisplay() {
+    if (widget.characterToShow != noShow) {}
+    return Image(height: 250, width: 250, image: AssetImage('bg.png'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: <Widget>[
-        Positioned(
-          left: 75,
-          top: 175,
-          child: const Image(
-            height: 250,
-            width: 250,
-            image: AssetImage('bg.png'),
-          ),
-        ),
-      ],
+      children: <Widget>[Positioned(left: 75, top: 175, child: _getDisplay())],
     );
   }
 }
