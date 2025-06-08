fa41cf3caaef9703b8f4769ad3d2bce73cde771b
vitalii boiko <vukelan@gmail.com>
2025-06-08 23:15:40 +0300

    covert to stateful widget

--- lib/src/hiragana/button_group.dart
+++ lib/src/hiragana/button_group.dart
@@ -3,21 +3,25 @@ import 'package:ltrainer/src/hiragana/display_letter.dart';
 import 'package:ltrainer/src/hiragana/left_buttons.dart';
 import 'package:ltrainer/src/hiragana/right_buttons.dart';
 
-class ButtonGroup extends StatelessWidget {
+const notSelected = -1;
+
+class ButtonGroup extends StatefulWidget {
   const ButtonGroup({super.key});
 
-  double _getLeft(int k) {
-    return 0;
-  }
+  State<ButtonGroup> createState() => _ButtonGroupState();
+}
 
-  double _calcTop(int k) {
-    return 100 + (k * 30);
-  }
+class _ButtonGroupState extends State<ButtonGroup> {
+  int leftButtonSelectedCharacter = notSelected;
 
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: <Widget>[LeftButtons(), RightButtons(), DisplayLetter()],
+      children: <Widget>[
+        LeftButtons(),
+        RightButtons(),
+        DisplayLetter(characterToShow: leftButtonSelectedCharacter),
+      ],
     );
   }
 }
