9d7ea79e9f382dd1026c0b9ccfcd817a96c69eb8
vitalii boiko <vukelan@gmail.com>
2025-06-11 02:35:26 +0300

    pass new videoFilename

--- lib/src/hiragana/button_group.dart
+++ lib/src/hiragana/button_group.dart
@@ -2,8 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:ltrainer/src/hiragana/display_letter.dart';
 import 'package:ltrainer/src/hiragana/left_buttons.dart';
 import 'package:ltrainer/src/hiragana/right_buttons.dart';
+import 'package:video_player/video_player.dart';
 
 const notSelected = -1;
+
+const defaultVideoFilename = 'a.mp4';
+
+String getVideoFilename(character) {
+  return '$character.mp4';
+}
+
+var leftCharacters = {
+  LeftButton.a.index: getVideoFilename(LeftButton.a.name),
+  LeftButton.u.index: getVideoFilename(LeftButton.u.name),
+  LeftButton.i.index: getVideoFilename(LeftButton.i.name),
+  LeftButton.e.index: getVideoFilename(LeftButton.e.name),
+  LeftButton.o.index: getVideoFilename(LeftButton.o.name),
+};
 
 class ButtonGroup extends StatefulWidget {
   const ButtonGroup({super.key});
@@ -14,10 +29,14 @@ class ButtonGroup extends StatefulWidget {
 
 class _ButtonGroupState extends State<ButtonGroup> {
   int characterToShow = notSelected;
+  String videoFilename = 'a.mp4';
 
   setCharacterToShow(int k) {
     setState(() {
       characterToShow = k;
+      if (k != notSelected) {
+        videoFilename = leftCharacters[k] ?? defaultVideoFilename;
+      }
     });
   }
 
@@ -27,7 +46,10 @@ class _ButtonGroupState extends State<ButtonGroup> {
       children: <Widget>[
         LeftButtons(setCharacterToShow: setCharacterToShow),
         RightButtons(),
-        DisplayLetter(characterToShow: characterToShow),
+        DisplayLetter(
+          characterToShow: characterToShow,
+          videoFilename: videoFilename,
+        ),
       ],
     );
   }
