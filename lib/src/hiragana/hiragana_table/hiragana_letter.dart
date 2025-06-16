08b9d02f228e7ccdf70a2a361aad8c5bf3d8dcc5
vitalii boiko <vukelan@gmail.com>
2025-06-17 02:49:30 +0300

    wrap into column

--- lib/src/hiragana/hiragana_table/hiragana_letter.dart
+++ lib/src/hiragana/hiragana_table/hiragana_letter.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class HiraganaLetter extends StatefulWidget {
-  const HiraganaLetter({super.key, required this.letter});
+  const HiraganaLetter({super.key, required this.letter, this.sound = ''});
 
   final String letter;
+  final String sound;
 
   @override
   State<HiraganaLetter> createState() => _HiraganaLetterState();
@@ -19,19 +20,31 @@ class _HiraganaLetterState extends State<HiraganaLetter> {
       decoration: BoxDecoration(
         color: Colors.white,
         border: Border.all(color: Colors.white),
-        borderRadius: BorderRadius.circular(20),
+        borderRadius: BorderRadius.circular(6),
         boxShadow: [
           BoxShadow(
             color: Colors.grey.withValues(alpha: 0.5),
-            offset: Offset(4, 4),
-            blurRadius: 8,
+            offset: Offset(2, 2),
+            blurRadius: 4,
           ),
         ],
       ),
-      child: Text(
-        widget.letter,
-        textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        spacing: 0,
+        children: [
+          Text(
+            widget.sound,
+            textAlign: TextAlign.start,
+            style: TextStyle(fontSize: 14, height: 0.6),
+          ),
+          Text(
+            widget.letter,
+            textAlign: TextAlign.start,
+            style: TextStyle(fontSize: 30),
+          ),
+        ],
       ),
     );
   }
