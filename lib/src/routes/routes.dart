778c7e57546ed8d2bec6460f1bba2735083a1111
vitalii boiko <vukelan@gmail.com>
2025-06-19 21:39:33 +0300

    add katakana page to routes

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:ltrainer/src/home.dart';
 import 'package:ltrainer/src/hiragana/page.dart';
+import 'package:ltrainer/src/katakana/page.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   '/hiragana': (BuildContext context) {
     return HiraganaPage();
+  },
+  '/katakana': (BuildContext context) {
+    return KatakanaPage();
   },
 };
