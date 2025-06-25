299c91de9191cf5500e1d9fccd3f5fdea39148c6
vitalii boiko <vukelan@gmail.com>
2025-06-25 21:30:42 +0300

    add display kanji

---
+++ lib/src/kanji/kanji_display.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:ltrainer/src/kanji/get_kanji.dart';
+import 'package:ltrainer/src/kanji/kanji_block.dart';
+
+class KanjiDisplay extends StatefulWidget {
+  const KanjiDisplay({super.key, required this.kanji});
+
+  final List<KanjiInfo> kanji;
+
+  @override
+  State<KanjiDisplay> createState() => _KanjiDisplayState();
+}
+
+class _KanjiDisplayState extends State<KanjiDisplay> {
+  List<Widget> _buildBlock() {
+    List<Widget> kanjiWidgets = [];
+    List<KanjiInfo> kanjiInfos = [];
+    for (var i = 0; i < widget.kanji.length; i++) {
+      kanjiInfos.add(widget.kanji[i]);
+      if (kanjiInfos.length == 7 || i == widget.kanji.length - 1) {
+        kanjiWidgets.add(KanjiBlock(kanjiList: kanjiInfos));
+        kanjiInfos = [];
+      }
+    }
+    return kanjiWidgets;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: _buildBlock());
+  }
+}
