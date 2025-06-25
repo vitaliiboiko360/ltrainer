df83008118e19e8c78f36ed942b1b2356f9562f0
vitalii boiko <vukelan@gmail.com>
2025-06-25 19:59:27 +0300

    add word container

---
+++ lib/src/kanji/kanji_wordcontainer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class KanjiWordContainer extends StatefulWidget {
+  const KanjiWordContainer({super.key});
+
+  @override
+  State<KanjiWordContainer> createState() => _KanjiWordContainerState();
+}
+
+class _KanjiWordContainerState extends State<KanjiWordContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: <Widget>[]);
+  }
+}
