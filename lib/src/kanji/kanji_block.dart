d1ff9576a4a3e22266cbee42d64ec803c9af2f8a
vitalii boiko <vukelan@gmail.com>
2025-06-25 21:41:22 +0300

    fill block with chars

--- lib/src/kanji/kanji_block.dart
+++ lib/src/kanji/kanji_block.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ltrainer/src/kanji/get_kanji.dart';
+import 'package:ltrainer/src/kanji/kanji_char.dart';
 
 class KanjiBlock extends StatefulWidget {
   const KanjiBlock({super.key, required this.kanjiList});
@@ -13,6 +14,28 @@ class KanjiBlock extends StatefulWidget {
 class _KanjiBlockState extends State<KanjiBlock> {
   @override
   Widget build(BuildContext context) {
-    return Row(children: <Widget>[Column()]);
+    return Row(
+      children: <Widget>[
+        Column(
+          children: [
+            KanjiChar(word: widget.kanjiList[0].kanji),
+            KanjiChar(word: widget.kanjiList[1].kanji),
+          ],
+        ),
+        Column(
+          children: [
+            KanjiChar(word: widget.kanjiList[2].kanji),
+            KanjiChar(word: widget.kanjiList[3].kanji),
+            KanjiChar(word: widget.kanjiList[4].kanji),
+          ],
+        ),
+        Column(
+          children: [
+            KanjiChar(word: widget.kanjiList[5].kanji),
+            KanjiChar(word: widget.kanjiList[6].kanji),
+          ],
+        ),
+      ],
+    );
   }
 }
