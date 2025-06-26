e205bde317c665014c955a6a4a9a55f5b944aa04
vitalii boiko <vukelan@gmail.com>
2025-06-26 19:22:22 +0300

    add buildChars

--- lib/src/kanji/kanji_block.dart
+++ lib/src/kanji/kanji_block.dart
@@ -12,10 +12,9 @@ class KanjiBlock extends StatefulWidget {
 }
 
 class _KanjiBlockState extends State<KanjiBlock> {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
+  List<Widget> _buildChars(kanjiList) {
+    if (kanjiList.length == 7) {
+      return [
         Column(
           children: [
             KanjiChar(word: widget.kanjiList[0].kanji),
@@ -35,7 +34,95 @@ class _KanjiBlockState extends State<KanjiBlock> {
             KanjiChar(word: widget.kanjiList[6].kanji),
           ],
         ),
-      ],
-    );
+      ];
+    }
+    if (kanjiList.length == 6) {
+      return [
+        Column(
+          children: [
+            KanjiChar(word: widget.kanjiList[0].kanji),
+            KanjiChar(word: widget.kanjiList[1].kanji),
+          ],
+        ),
+        Column(
+          children: [
+            KanjiChar(word: widget.kanjiList[2].kanji),
+            SizedBox(height: 52),
+            KanjiChar(word: widget.kanjiList[3].kanji),
+          ],
+        ),
+        Column(
+          children: [
+            KanjiChar(word: widget.kanjiList[4].kanji),
+            KanjiChar(word: widget.kanjiList[5].kanji),
+          ],
+        ),
+      ];
+    }
+
+    if (kanjiList.length == 5) {
+      return [
+        Column(
+          children: [
+            KanjiChar(word: widget.kanjiList[0].kanji),
+            KanjiChar(word: widget.kanjiList[1].kanji),
+          ],
+        ),
+        Column(
+          children: [
+            KanjiChar(word: widget.kanjiList[2].kanji),
+            SizedBox(height: 52),
+            KanjiChar(word: widget.kanjiList[3].kanji),
+          ],
+        ),
+        Column(children: [KanjiChar(word: widget.kanjiList[4].kanji)]),
+      ];
+    }
+    if (kanjiList.length == 4) {
+      return [
+        Column(
+          children: [
+            KanjiChar(word: widget.kanjiList[0].kanji),
+            KanjiChar(word: widget.kanjiList[1].kanji),
+          ],
+        ),
+        Column(
+          children: [
+            KanjiChar(word: widget.kanjiList[2].kanji),
+            KanjiChar(word: widget.kanjiList[3].kanji),
+          ],
+        ),
+      ];
+    }
+
+    bool isInSize(List<KanjiInfo> list, int size) {
+      return list.length > size;
+    }
+
+    return [
+      Column(
+        children: [
+          KanjiChar(word: widget.kanjiList[0].kanji),
+          isInSize(widget.kanjiList, 1)
+              ? KanjiChar(word: widget.kanjiList[1].kanji)
+              : SizedBox.shrink(),
+        ],
+      ),
+      isInSize(widget.kanjiList, 2)
+          ? Column(
+            children: [
+              KanjiChar(word: widget.kanjiList[2].kanji),
+              isInSize(widget.kanjiList, 3)
+                  ? KanjiChar(word: widget.kanjiList[3].kanji)
+                  : SizedBox.shrink(),
+            ],
+          )
+          : SizedBox.shrink(),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: _buildChars(widget.kanjiList));
   }
 }
