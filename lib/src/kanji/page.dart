0a401d8ac4b9da892536653995361af8f84c8af9
vitalii boiko <vukelan@gmail.com>
2025-06-27 13:19:26 +0300

    blur background

--- lib/src/kanji/page.dart
+++ lib/src/kanji/page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:ltrainer/src/back_home.dart';
 import 'package:ltrainer/src/kanji/main_screen.dart';
@@ -17,17 +19,25 @@ class _KanjiPageState extends State<KanjiPage> {
         child: Center(
           child: SizedBox(
             width: 600,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Transform(
-                  transform: Matrix4.translationValues(-160.0, 20.0, 0.0),
-                  child: BackHome(),
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(85, 227, 234, 255),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Transform(
+                      transform: Matrix4.translationValues(-160.0, 20.0, 0.0),
+                      child: BackHome(),
+                    ),
+                    Text('Kanji Drill', style: TextStyle(fontSize: 30)),
+                    MainScreen(),
+                  ],
                 ),
-                Text('Kanji Drill', style: TextStyle(fontSize: 30)),
-                MainScreen(),
-              ],
+              ),
             ),
           ),
         ),
