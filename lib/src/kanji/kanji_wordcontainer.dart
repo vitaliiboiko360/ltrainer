9abda0bab598bd6264604ea996f9854f3d2b4bab
vitalii boiko <vukelan@gmail.com>
2025-06-28 02:38:54 +0300

    display kanji info

--- lib/src/kanji/kanji_wordcontainer.dart
+++ lib/src/kanji/kanji_wordcontainer.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:ltrainer/src/kanji/get_kanji.dart';
 
 class KanjiWordContainer extends StatefulWidget {
-  const KanjiWordContainer({super.key});
+  const KanjiWordContainer({super.key, required this.kanjiInfo});
+
+  final KanjiInfo kanjiInfo;
 
   @override
   State<KanjiWordContainer> createState() => _KanjiWordContainerState();
@@ -10,6 +13,27 @@ class KanjiWordContainer extends StatefulWidget {
 class _KanjiWordContainerState extends State<KanjiWordContainer> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox.shrink();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      spacing: 8,
+      children: [
+        Text(
+          widget.kanjiInfo.sound,
+          style: TextStyle(fontSize: 22, color: Colors.black),
+        ),
+        Text(
+          widget.kanjiInfo.kunyomi,
+          style: TextStyle(fontSize: 22, color: Colors.black),
+        ),
+        Text(
+          widget.kanjiInfo.kanji,
+          style: TextStyle(fontSize: 42, color: Colors.black),
+        ),
+        Text(
+          widget.kanjiInfo.translation,
+          style: TextStyle(fontSize: 22, color: Colors.black),
+        ),
+      ],
+    );
   }
 }
