3896612e077fc33f94e26a969f29b17c961aad34
vitalii boiko <vukelan@gmail.com>
2025-07-09 16:03:24 +0300

    add on story route with arguments

---
+++ lib/src/stories/story/story_route.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:ltrainer/src/stories/story/page.dart';
+
+onStoryRoute(settings) {
+  if (settings.name.startWith(StoryRoute.routePrefix)) {
+    final args = settings.arguments as StoryRouteArguments;
+    return MaterialPageRoute(
+      builder: (context) {
+        return StoryPage(urlDir: args.urlDir);
+      },
+    );
+  }
+  return null;
+}
+
+class StoryRoute {
+  static String routePrefix = '/stories';
+}
+
+class StoryRouteArguments {
+  final String urlDir;
+
+  StoryRouteArguments(this.urlDir);
+}
