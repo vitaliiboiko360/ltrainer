d729e278507b3c7d95e5aa5cd1cec2fd88d19ff5
vitalii boiko <vukelan@gmail.com>
2025-07-10 18:48:50 +0300

    pass dir url to display

--- lib/src/stories/story/display_text.dart
+++ lib/src/stories/story/display_text.dart
@@ -3,16 +3,16 @@ import 'package:ltrainer/src/stories/story/display_lines_images.dart';
 import 'package:ltrainer/src/stories/story/get_text.dart';
 import 'package:http/http.dart' as http;
 
-class TextAudio extends StatefulWidget {
-  const TextAudio({super.key, required this.dirUrl});
+class DisplayText extends StatefulWidget {
+  const DisplayText({super.key, required this.dirUrl});
 
   final String dirUrl;
 
   @override
-  State<TextAudio> createState() => _TextAudioState();
+  State<DisplayText> createState() => _DisplayTextState();
 }
 
-class _TextAudioState extends State<TextAudio> {
+class _DisplayTextState extends State<DisplayText> {
   late Future<List<LineInfo>> lineInfoList;
 
   @override
@@ -25,15 +25,18 @@ class _TextAudioState extends State<TextAudio> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<LineInfo>>(
-    future: lineInfoList,
-    builder: (context, snapshot) {
-      if (snapshot.hasData) {
-        return DisplayLinesImages(content: snapshot.data!);
-      } else if (snapshot.hasError) {
-        return Text('${snapshot.error}');
-      }
-      return const CircularProgressIndicator();
-    },
-  )
+      future: lineInfoList,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return DisplayLinesImages(
+            content: snapshot.data!,
+            dirUrl: widget.dirUrl,
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+        return const CircularProgressIndicator();
+      },
+    );
   }
 }
