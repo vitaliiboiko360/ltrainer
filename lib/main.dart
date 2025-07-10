4cbb3e64d6c902a81d9825463d722ddbacef2748
vitalii boiko <vukelan@gmail.com>
2025-07-10 18:49:17 +0300

    add path url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:ltrainer/src/app.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(const App());
 }
