9c6a6a5df62592ccadcd0e12335088ee420bb178
vitalii boiko <vukelan@gmail.com>
2025-07-11 01:26:36 +0300

    add top bottom padding to text

--- lib/src/stories/story/text_line.dart
+++ lib/src/stories/story/text_line.dart
@@ -17,17 +17,20 @@ class TextLine extends StatelessWidget {
     for (var i = 0; i < japaneseChars.length; i++) {
       final index = hiraganaInfoList.indexWhere((elm) => elm.place == i);
       columns.add(
-        Column(
-          children: [
-            Text(
-              index != -1 ? hiraganaInfoList[i].hiragana : ' ',
-              style: TextStyle(fontSize: 12),
-            ),
-            Text(
-              japaneseChars.characterAt(i).toString(),
-              style: TextStyle(fontSize: 28),
-            ),
-          ],
+        Padding(
+          padding: EdgeInsetsGeometry.directional(top: 15, bottom: 5),
+          child: Column(
+            children: [
+              Text(
+                index != -1 ? hiraganaInfoList[i].hiragana : ' ',
+                style: TextStyle(fontSize: 12),
+              ),
+              Text(
+                japaneseChars.characterAt(i).toString(),
+                style: TextStyle(fontSize: 32),
+              ),
+            ],
+          ),
         ),
       );
     }
@@ -36,6 +39,9 @@ class TextLine extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: buildTextLine());
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: buildTextLine(),
+    );
   }
 }
